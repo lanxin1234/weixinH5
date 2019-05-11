af7a6b354d3ef99eb249c1f896d038f2c49e1d70
daotech <daotech@daotech.io>
2019-05-11 19:38:44 +0800

    init

---
+++ src/components/login/login.vue
@@ -0,0 +1,17 @@
+<template>
+<div class='login'>
+  <h2>这是partial
+  </h2>
+</div>
+</template>
+<script>
+export default {
+  name: 'login',
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+@import './login.scss';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'login',
+    component: login
+  }]
 })
