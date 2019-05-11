807373ccaa49e2b948a4e1771f572c2c41e7686c
daotech <daotech@daotech.io>
2019-05-11 21:58:25 +0800

    updata

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,14 +1,22 @@
 <template>
-<div class='login'>
+<div class='login' ref='login'>
   <h2>这个登录页面</h2>
 </div>
 </template>
 <script>
+import material from './../../../static/material.json'
 export default {
   name: 'login',
   data: () => ({
 
-  })
+  }),
+  created() {
+
+  },
+  mounted() {
+    this.$refs.login.style.background = material
+    console.log('login', this.$refs.login)
+  }
 }
 </script>
 <style lang="scss" scoped>
